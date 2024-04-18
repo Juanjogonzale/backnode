ec848a809fdab782201f45f190c056e0375c4bcb
Duvan Rodrigue <duvanandresrodriguez1996@hotmail.com>
2024-04-18 00:17:33 -0500

    comit

--- src/controllers/usuariosController/usuariosController.js
+++ src/controllers/usuariosController/usuariosController.js
@@ -7,14 +7,9 @@ async function guardarUsuario(req, res) {
     try {
         const query = 'INSERT INTO usuarios (nombres, apellidos, cedula, email, password, direccion, ciudad, rol, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
         const values = [nombres, apellidos, cedula, email, password, direccion, ciudad, rol, estado];
-        db.query(query, values, (err, result) => {
-            if (err) {
-                console.error('Error al guardar el usuario:', error);
-                res.status(500).json({ error: 'Error interno del servidor' });
-                return;
-            }
-            res.status(201).json(result);
-        });
+        const result = await db.query(query, values);
+
+        res.status(201).json(result.rows[0]);
     } catch (error) {
         console.error('Error al guardar el usuario:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
@@ -25,19 +20,15 @@ async function guardarUsuario(req, res) {
 async function obtenerUsuarios(req, res) {
     try {
         const query = 'SELECT * FROM usuarios';
-        db.query(query, (err, result) => {
-            if (err) {
-                console.error('Error al obtener los usuarios:', err);
-                res.status(500).json({ error: 'Error interno del servidor' });
-                return;
-            }
-            res.status(200).json(result);
-        });
-    } catch (error) {
+        const result = await db.query(query);
+    
+        res.status(201).json(result.rows);
+      } catch (error) {
         console.error('Error al obtener los usuarios:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
+      }
     }
-}
+
 
 module.exports = {
     guardarUsuario,
